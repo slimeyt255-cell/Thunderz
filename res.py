254d09ba59c16f3810ab53b904c5da20050ffb87
slimeyt255-cell <slimeyt255@gmail.com>
2026-09-18 16:08:02 +0700

    Update res.py

--- res.py
+++ res.py
@@ -7,7 +7,7 @@ json  = {"user_id": "1073383604576591974",
     "guild_id": "1077968892535775262",
     "query": "zoe azul"}
 if n == 1:
-    url = requests.post("https://bdscriptmusic-production.up.railway.app/play-music", json=json)
+    url = requests.post("https://ten-service.onrender.com/play-music", json=json)
 
 
 print(url.text)
